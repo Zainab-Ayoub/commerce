4e7c0b17b74d9df28532b6d98246e1709147b7f2
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-04-06 18:01:26 +0500

    form for auctionlisting

--- auctions/models.py
+++ auctions/models.py
@@ -9,15 +9,17 @@ class AuctionListing(models.Model):
     title = models.CharField(max_length=80)
     description = models.TextField()
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    image_url = models.URLField(blank = True, null = True)
+    category = models.CharField(max_length = 80, blank = True, null = True)
+
     def __str__(self):
         return self.title    
 
 class Bids(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
-    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
-     
+    bid_amount = models.DecimalField(max_digits = 10, decimal_places = 2)
+    
     class Meta:
         verbose_name_plural = "Bids"
         
